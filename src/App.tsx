c59b91c92bbc68ea9faedcb0c6103282ae8fc78d
Richard <richard@stromer.org>
2022-11-03 19:29:16 -0700

    remove stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import React from "react";
 import { useAppSelector, useAppDispatch } from "./hooks";
-import logo from "./logo.svg";
-import "./App.css";
 
 import { loadDataset } from "./reducers/dataset";
 
@@ -11,18 +8,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <button
           onClick={() =>
             dispatch(loadDataset("/datasets/energy_dataset_small.csv"))
