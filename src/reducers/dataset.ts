8f1a1a839e9ef9eb428d974bf4e68d608a1cf852
Richard <richard@stromer.org>
2022-11-03 19:41:34 -0700

    get redux thunk working

--- src/reducers/dataset.ts
+++ src/reducers/dataset.ts
@@ -1,16 +1,43 @@
-import { createSlice } from "@reduxjs/toolkit";
-import type { PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+// import type { PayloadAction } from "@reduxjs/toolkit";
 
 export const datasetSlice = createSlice({
   name: "dataset",
-  initialState: {},
-  reducers: {
-    loadDataset: (state, action: PayloadAction<string>) => {
-      console.log(action);
-    },
+  initialState: {
+    status: "idle",
+    data: [],
+  },
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(loadDataset.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(loadDataset.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.data = action.payload as any;
+      });
+    // .addCase(loadDataset.rejected, (state, action) => {
+    //   state.status = "failed";
+    //   state.error = action.error.message;
+    // });
   },
 });
 
-export const { loadDataset } = datasetSlice.actions;
+export const loadDataset = createAsyncThunk<Object, string>(
+  "dataset/loadDataset",
+  async (url, thunkAPI) => {
+    console.log(url);
+    // const response = await client.get("/fakeApi/posts");
+    // return response.data;
+    function wait(milliseconds: number) {
+      return new Promise((resolve) => setTimeout(resolve, milliseconds));
+    }
+    await wait(3000);
+    return [{ hi: "world" }];
+  }
+);
+
+// export const { loadDataset } = datasetSlice.actions;
 
 export default datasetSlice.reducer;
